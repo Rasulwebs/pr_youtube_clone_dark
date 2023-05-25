ecf91a6669ad2b2e4ee188c2be5a7f601bdd4126
Rasulwebs <boboqulovrasul80@gmail.com>
2023-05-25 13:13:18 +0500

    creating new youtube clone app

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from "react-router-dom"; 
+import { Typography, Card, CardContent, CardMedia, CardActionArea } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
+  <Card sx={{ width: 345 }}>
+    <CardActionArea>
+    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
+      <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
+        sx={{ height: 190 }} 
+        />
+    </Link>
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+        </Typography>
+      </Link>
+    </CardContent>
+        </CardActionArea>
+  </Card>
+);
+
+export default VideoCard
